c632fe6a644b0e52fd9bf9f2806c587fffa156f7
pietrop <pietro.passarelli@gmail.com>
2016-10-08 22:39:51 -0400

    upadted deploy script so that toolbar does not show in deployed app

--- deploy.js
+++ deploy.js
@@ -13,10 +13,20 @@ var spawn = require('child_process').spawn;
 
 // }
 
-
+////////////////////////////Hiding toolbar for deployment
+var packageJsonFile = "./package.json";
+//read package.json file 
+var packageJson = require(packageJsonFile);
+//hide toolbar in window preferences
+packageJson.window.toolbar = false;
+//write file back to system.
+fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null,  4))
+//deploy code 
+///////////////////////////// Deploying/building app 
 var NwBuilder = require('nw-builder');
 var nw = new NwBuilder({
-  files: ['./**'], // simple-glob format
+  // files: ['./**'], // simple-glob format
+    files: ['./frontEnd/**','./interactive_transcription_generator/**','./node_modules/**','./config.js','./package.json' ], // simple-glob format
   // platforms: ['osx32', 'osx64', 'win32', 'win64', 'linux32', 'linux64']
   platforms: ['osx64'],
   version: '0.12.3'
@@ -24,6 +34,22 @@ var nw = new NwBuilder({
 
 nw.build().then(function () {
   console.log('Finished packaging the app');
+  	//////////////////////////showing toolbar for development
+	//on deploy finish 
+	//change to show toolbar
+	packageJson.window.toolbar = true;
+	//write file back down to disk 
+	fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null,  4))
+	////////////////////////
 }).catch(function (error) {
   console.error(error);
+  	//////////////////////////showing toolbar for development
+	//on deploy finish 
+	//change to show toolbar
+	packageJson.window.toolbar = true;
+	//write file back down to disk 
+	fs.writeFileSync(packageJsonFile, JSON.stringify(packageJson, null,  4))
+	////////////////////////
 });
+
+
