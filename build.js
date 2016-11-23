ff1172e204a5059f4126ead108be155f30784f7c
pietrop <pietro.passarelli@gmail.com>
2016-11-23 15:26:23 -0500

    cleand up build.js removing ocmmented out dmg code

--- build.js
+++ build.js
@@ -20,10 +20,11 @@ var nw = new NwBuilder({
     './nwjs/**', './lib/**', './node_modules/**', './config.js', './package.json'
   ],
   //for now only osx64 as need to recompile ffmpeg for other versions.
-  platforms: ['osx64', 'win64'], // ['osx32', 'osx64', 'win32', 'win64', 'linux32', 'linux64']
+  platforms: ['osx64'], // ['osx32', 'osx64', 'win32', 'win64', 'linux32', 'linux64']
   // https://github.com/nwjs/nw-builder#optionsmacicns
   macIcns: 'assets/nw.icns',
-  version: '0.12.3', // of nwjs to use
+  // version of nwjs to use
+  version: '0.12.3', 
   buildDir: buildDir
 });
 
@@ -34,59 +35,6 @@ console.log('About to package the app');
 
 nw.build().then(function () {
   console.log('Finished packaging the app');
-  /**
-   * packaging app in dmg for os x, and placing it on the Desktop.
-   * `npm install appdmg  -g` might require to be installed globally for it to work
-   * appdmg appdmg.json ~/Desktop/autoEdit2.dmg
-   * seems like this could be the issue https://github.com/LinusU/node-appdmg/issues/63
-   */
-  ///////////////////////////////////////////////////////
-  //// or spawn process
-  //// appdmg appdmg.json ~/Desktop/autoEdit2.dmg
-  // var spawn = require('child_process').spawn;
-  // var appdmg = spawn('appdmg', ['./appdmg.json', '~/Desktop/autoEdit2.dmg']);
-
-  // appdmg.stdout.on('data', function(data) {
-  //   console.log("stdout: " +data);
-  // });
-
-  // appdmg.stderr.on('data', function(data) {
-  //   console.log("stderr:" +data);
-  // });
-
-  // appdmg.on('close', function(code)  {
-  //   console.log("child process exited with code" +code);
-  // });
-  ///////////////////////////////////////////////////////
-  // var appdmg = require('appdmg');
-  // var ee = appdmg({ source: './appdmg.json', target: '~/Desktop/autoEdit2.dmg' });
-
-  // ee.on('progress', function (info) {
-  //    console.log("progress")
-  //   // console.log(info.current, into.total, info.type)
-  //   // info.current is the current step
-  //   // info.total is the total number of steps
-  //   // info.type is on of 'step-begin', 'step-end'
-
-  //   // 'step-begin'
-  //   // info.title is the title of the current step
-
-  //   // 'step-end'
-  //   // info.status is one of 'ok', 'skip', 'fail'
-  // });
-
-  // ee.on('finish', function () {
-  //   // There now is a `test.dmg` file
-  //   console.log("check desktop for .dmg file")
-  // });
-
-  // ee.on('error', function (err) {
-  //   // An error occurred
-  //   console.log("error")
-  //   console.log(err)
-  // });
-
-
 }).catch(function (error) {
   console.error(error);
 });
