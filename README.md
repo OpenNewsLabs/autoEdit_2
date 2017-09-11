c6c048b031289bc976787a14967a82d66391d063
pietrop <pietro.passarelli@gmail.com>
2017-09-11 11:19:10 -0300

    commiting in branch google stt

--- README.md
+++ README.md
@@ -127,3 +127,38 @@ List of contributors that have helped shaped this version of autoEdit by contrib
 
 
 
+<!-- 
+to work with google speech to text module
+
+https://stackoverflow.com/questions/42616008/node-module-version-conflict-when-installing-modules-for-electron
+
+
+npm install --save-dev electron-rebuild
+
+
+./node_modules/.bin/electron-rebuild
+
+
+ENV var
+```
+# Electron's version.
+export npm_config_target=1.6.11
+# The architecture of Electron, can be ia32 or x64.
+export npm_config_arch=x64
+export npm_config_target_arch=x64
+# Download headers for Electron.
+export npm_config_disturl=https://atom.io/download/electron
+# Tell node-pre-gyp that we are building for Electron.
+export npm_config_runtime=electron
+# Tell node-pre-gyp to build module from source code.
+export npm_config_build_from_source=true
+# Install all dependencies, and store cache to ~/.electron-gyp.
+HOME=~/.electron-gyp npm install
+
+```
+
+npm rebuild
+
+
+ -->
+
--- electron/google_keys.js
+++ electron/google_keys.js
@@ -2,24 +2,17 @@ var fs = require('fs');
 var electron = require('electron');
 var currentWindow = electron.remote.getCurrentWindow();
 
+var googleKeysPath;
+
 if (window.process !== 'undefined') {
-  var googleKeysPath = currentWindow.dataPath + '/gckeys.json';
+   googleKeysPath = currentWindow.dataPath + '/gckeys.json';
 }else{
   //not in nwjs 
-  var googleKeysPath = "/";
+   googleKeysPath = "/";
 }
 
 var googleKeys = {
-  "type": "",
-  "project_id": "",
-  "private_key_id": "",
-  "private_key": "",
-  "client_email": "",
-  "client_id": "",
-  "auth_uri": "",
-  "token_uri": "",
-  "auth_provider_x509_cert_url": "",
-  "client_x509_cert_url": ""
+  "apiKey": ""
 }
 
 var googleKeysSet = false;
@@ -59,18 +52,18 @@ function getGoogleAPIKeysPath(){
 //set
 function setGoogleAPIkeys(keys){
   // then assuming is a file path 
-  if(typeof keys == 'string'){
-    //TODO change to read.
-    const fs = require('fs');
-        keys = JSON.parse(fs.readFileSync(keys, 'utf8'));
-    // keys = require('keys');
-  }
-  if(keysAreValid(keys)){
+  // if(typeof keys == 'string'){
+  //   //TODO change to read.
+  //   const fs = require('fs');
+  //       keys = JSON.parse(fs.readFileSync(keys, 'utf8'));
+  //   // keys = require('keys');
+  // }
+  // if(keysAreValid(keys)){
      fs.writeFileSync(googleKeysPath, JSON.stringify(keys));
-    }else{
+    // }else{
     // not setting keys. should add some error handling it, but
     // at the moment validation check is handled in view
-   }
+   // }
 }
 
 //check if they are set  
