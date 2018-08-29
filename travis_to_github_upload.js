73038f32a8630c382b11edbf96162dbcb1c751b8
pietrop <pietro.passarelli@gmail.com>
2018-08-28 23:40:40 -0400

    testing windows

--- travis_to_github_upload.js
+++ travis_to_github_upload.js
@@ -1,11 +1,8 @@
 
-// Travis tag from package.json
+// takes file to upload as script param
+// knows release name & tag name from ENV var
+
 const packageJson = require('./package.json');
-var TRAVIS_TAG = packageJson.version;
-
-console.log('TRAVIS_TAG ',TRAVIS_TAG);
-
-var UPLOADTOOL_SUFFIX = packageJson.name;
 
 console.log('UPLOADTOOL_SUFFIX ',UPLOADTOOL_SUFFIX);
 console.log('process.env.TRAVIS_OS_NAME',process.env.TRAVIS_OS_NAME);
@@ -13,3 +10,25 @@ console.log('process.env.TRAVIS_OS_NAME',process.env.TRAVIS_OS_NAME);
 var RELEASE_NAME=`${UPLOADTOOL_SUFFIX}-${TRAVIS_TAG}-${process.env.TRAVIS_OS_NAME}`
 var RELEASE_TITLE=`${UPLOADTOOL_SUFFIX} ${TRAVIS_TAG} ${process.env.TRAVIS_OS_NAME}`
 var is_prerelease=`${process.env.IS_PRERELEASE}`
+
+console.log('RELEASE_NAME ',RELEASE_NAME);
+console.log('RELEASE_TITLE ',RELEASE_TITLE);
+
+// GITHUB: get list of release from githuhb 
+
+
+    // tag name is not in list of releases
+
+        // GITHUB: create new release
+            // with body description?
+
+        // add asset to this release 
+
+
+    // if the tag name is in the list of releases
+
+        // GITHUB: delete asset
+
+        // GITHUB: upload new asset 
+
+        // GITHUB: edit body description - optional 
